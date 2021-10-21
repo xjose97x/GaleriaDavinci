96580c4cf44b8fc57f3729f040c6701a73a9b4b2
Jose Escudero <joseignacioescudero97@hotmail.com>
2021-10-21 15:13:33 -0500

    ejemplo francisco

--- GaleriaDavinci.Web/Models/ErrorViewModel.cs
+++ GaleriaDavinci.Web/Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace GaleriaDavinci.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        //hola mundo
     }
 }
