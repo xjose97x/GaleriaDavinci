62accb1dfc88f62925f8fd03a84bf87929d01e85
Jose Escudero <joseignacioescudero97@hotmail.com>
2022-01-13 13:24:35 -0500

    Crear proyectos Xamarin

---
+++ GaleriaDavinci.Mobile/GaleriaDavinci.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using GaleriaDavinci.Mobile.ViewModels;
+using GaleriaDavinci.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GaleriaDavinci.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ GaleriaDavinci.Mobile/GaleriaDavinci.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GaleriaDavinci.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GaleriaDavinci.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
