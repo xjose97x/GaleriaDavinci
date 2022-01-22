6516ecec8e961da78e41f429095ef12a3ff59abd
ChristianSama <christiansama1601@gmail.com>
2022-01-21 22:40:23 -0500

    Mobile: GalleryItemPage UI Inicial

--- GaleriaDavinci.Mobile/GaleriaDavinci.Mobile/ViewModels/GalleryItemViewModel.cs
+++ GaleriaDavinci.Mobile/GaleriaDavinci.Mobile/ViewModels/GalleryItemViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using GaleriaDavinci.Mobile.Services;
 using GaleriaDavinci.Shared.Dto;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -10,6 +13,17 @@ namespace GaleriaDavinci.Mobile.ViewModels
     public class GalleryItemViewModel : INotifyPropertyChanged
     {
         public IGalleryApiService GalleryApiService { get; }
+
+        ArtPieceDto artPieceDto;
+        public ArtPieceDto ArtPieceDto {
+            get => artPieceDto;
+            set {
+                if (artPieceDto != value) {
+                    artPieceDto = value;
+                    OnPropertyChanged(nameof(ArtPieceDto));
+                }
+            }
+        }
 
         string title = string.Empty;
         public string Title
@@ -25,30 +39,76 @@ namespace GaleriaDavinci.Mobile.ViewModels
             }
         }
 
-        ArtPieceDto artPieceDto;
-        public ArtPieceDto ArtPieceDto
-        {
-            get => artPieceDto;
-            set
-            {
-                if (artPieceDto != value)
-                {
-                    artPieceDto = value;
-                    Title = artPieceDto.Name;
-                    OnPropertyChanged(nameof(ArtPieceDto));
+        ImageSource artPieceImage;
+        public ImageSource ArtPieceImage {
+            get => artPieceImage;
+            set {
+                if (artPieceImage != value) {
+                    artPieceImage = value;
+                    OnPropertyChanged(nameof(ArtPieceImage));
                 }
             }
         }
 
-        public async Task LoadArtpiece(int artPieceId)
-        {
+        string description = string.Empty;
+        public string Description {
+            get => description;
+            set {
+                if (description != value) {
+                    description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        string author;
+        public string Author {
+            get => author;
+            set {
+                if (author != value) {
+                    author = value;
+                    OnPropertyChanged(nameof(Author));
+                }
+            }
+        }
+
+        int year;
+        public int Year {
+            get => year;
+            set {
+                if (year != value) {
+                    year = value;
+                    OnPropertyChanged(nameof(Year));
+                }
+            }
+        }
+
+        ObservableCollection<ReviewDto> reviews;
+
+        public ObservableCollection<ReviewDto> Reviews {
+            get => reviews;
+            set {
+                if (reviews != value) {
+                    reviews = value;
+                    OnPropertyChanged(nameof(Reviews));
+                }
+            }
+        }
+
+        public async Task LoadArtpiece(int artPieceId) {
             ArtPieceDto = await GalleryApiService.GetArtPiece(artPieceId);
+            Title = artPieceDto.Name;
+            ArtPieceImage = Helpers.Base64ToImage(artPieceDto.Url);
+            Description = artPieceDto.Description;
+            Author = artPieceDto.AuthorName;
+            Year = artPieceDto.Year;
+            Reviews = new ObservableCollection<ReviewDto>(artPieceDto.Reviews);
+            int x = 1;
         }
 
         public GalleryItemViewModel()
         {
             GalleryApiService = DependencyService.Get<IGalleryApiService>();
-            Title = "Detalle obra";
         }
 
         #region INotifyPropertyChanged
